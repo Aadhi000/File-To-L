eb47e7d52a40593a2deb2f3f073d149d65b2fc4d
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-25 20:15:48 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -10,7 +10,7 @@ class Var(object):
     API_ID = int(getenv('API_ID'))
     API_HASH = str(getenv('API_HASH'))
     BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'TeleRoidFile2LinkBot'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'OpusTechz'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
