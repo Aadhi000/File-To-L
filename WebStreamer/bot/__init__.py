e1b6d0677e5ff108c731d12e228ba46f5d602e20
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-27 00:32:24 +0530

    Update __init__.py

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -4,11 +4,11 @@
 from pyrogram import Client
 from ..vars import Var
 
-StreamBot = Client(
+StreamBot = client(
     SESSION_NAME=Var.SESSION_NAME,
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
-)
+    )
