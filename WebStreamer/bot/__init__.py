2162909b6ecd6869a1adf25859c3c8062520bde9
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-04-26 22:34:05 +0530

    Update __init__.py

--- WebStreamer/bot/__init__.py
+++ WebStreamer/bot/__init__.py
@@ -5,7 +5,6 @@ from pyrogram import Client
 from ..vars import Var
 
 StreamBot = Client(
-    session_name='Web Streamer',
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
     bot_token=Var.BOT_TOKEN,
